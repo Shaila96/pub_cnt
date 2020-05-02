f046d994f20983b2c52724f85598ec2766815717
Shaila Zaman <shaila.zaman96@gmail.com>
2020-05-02 18:44:59 -0500

    fixed init

--- pub_cnt/__init__.py
+++ pub_cnt/__init__.py
@@ -1,5 +1,5 @@
 import sys, os
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'pub_cnt'))
+# sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'pub_cnt'))
 
-from altmetric import Altmetric
+from pub_cnt.altmetric import Altmetric
