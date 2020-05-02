b4936250372e1cf1a98b1ed39f5e95cb3556a668
Shaila Zaman <shaila.zaman96@gmail.com>
2020-05-02 15:49:13 -0500

    .gitignore is now working

--- pub_cnt/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-setup(name='pub-cnt',
-  description='pub-cnt',
-  author='Shaila Zaman, Emtiaz Ahmed',
-  version='0.1',
-  install_requires=['coverage>=3.7.1', 'nose>=1.3.7', 'radon>=3.0.1'])
