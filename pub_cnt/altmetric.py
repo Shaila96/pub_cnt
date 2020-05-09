0cd10f7509508572ea3f3861a116b0dae4d0a46e
Shaila Zaman <shaila.zaman96@gmail.com>
2020-05-09 17:33:02 -0500

    handled multiple entyr

--- pub_cnt/altmetric.py
+++ pub_cnt/altmetric.py
@@ -2,6 +2,7 @@ import logging
 import requests, json
 import pandas as pd
 import sys
+import time
 
 def parse_data(data, keys):
   try:
@@ -11,10 +12,11 @@ def parse_data(data, keys):
   except:
     return 'Not Available'
 
-def construct_data_frame(data):
+def construct_data_frame(data, _id):
   get_value = lambda field: [data[field]] if field in data.keys() else 'Not Available'
   
   df = pd.DataFrame({
+    'input_id': _id,
     'title': get_value('title'),
     'journal': get_value('journal'),
     'doi': get_value('doi'),
@@ -31,37 +33,49 @@ def construct_data_frame(data):
     'type': get_value('type'),
     'altmetric_id': get_value('altmetric_id'),
     'publication_url': get_value('url')
-  })
+  }, index = [0])
   
   return df
 
-def get_url(doi, pmid):
-  construct_url = lambda parameter, _id: 'https://api.altmetric.com/v1/%s/%s' % (parameter, str(_id))
-  
+def handle_user_input(doi = None, pmid = None, dois = None, pmids = None):
   if doi:
-    return construct_url('doi', doi)
-  else:
-    return construct_url('pmid', pmid)
-
-def get_data(doi = None, pmid = None, dois=None, pmids=None):
-  try:
-    if not doi and not pmid:
-      logging.error('doi or pubmed id must be provided')
-      return
-    
-    response = requests.get(get_url(doi, pmid), headers = {'Accept': 'application/json'})
-    
-    if response.status_code == 200:
-      try:
-        json_data = json.loads(response.text.encode('utf-8'))
-      except:
-        logging.error("json data not available")
-        return
-      
-      return construct_data_frame(json_data)
-    
-    else:
-      logging.error('Response Error: %s', response.status_code)
+    return 'doi', [str(doi)]
+  if pmid:
+    return 'pmid', [str(pmid)]
+  if dois:
+    return 'doi', list(map(str, dois))
+  if dois:
+    return 'pmid', list(map(str, pmids))
   
-  except:
-    logging.error('Error occurred unavailable due to %s' % sys.exc_info()[0].__name__)
+  return '', list('')
+
+def get_data(doi = None, pmid = None, dois = None, pmids = None):
+  id_type, ids = handle_user_input(doi, pmid, dois, pmids)
+  result_df = pd.DataFrame({})
+  
+  for i, _id in enumerate(ids):
+    try:
+      response = requests.get(lambda id_type, _id:
+      'https://api.altmetric.com/v1/%s/%s' % (id_type, _id),
+        headers = {'Accept': 'application/json'})
+      
+      # if response.status_code == 429:
+      #   print("Rate is limited and you finished your rate")
+      #   time.sleep(3600)
+      #   i = i - 1
+      #   continue
+      # elif response.status_code == 502:
+      #   print("API under  maintenance")
+      #   time.sleep(3600)
+      #   i = i - 1
+      #   continue
+      
+      json_data = json.loads(response.text.encode('utf-8'))
+    
+    except:
+      logging.error('Data unavailable for %s:%s due to %s' % (id_type, _id, sys.exc_info()[0].__name__))
+      json_data = {}
+
+    result_df = result_df.append(construct_data_frame(json_data, _id))
+  
+  return result_df
