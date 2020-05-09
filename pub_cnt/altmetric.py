dd63a725cee2cca4b080b544b33f9912cb01f633
Shaila Zaman <shaila.zaman96@gmail.com>
2020-05-09 17:37:53 -0500

    handled api issues

--- pub_cnt/altmetric.py
+++ pub_cnt/altmetric.py
@@ -49,33 +49,35 @@ def handle_user_input(doi = None, pmid = None, dois = None, pmids = None):
   
   return '', list('')
 
+def get_response():
+  return requests.get(lambda id_type, _id:
+  'https://api.altmetric.com/v1/%s/%s' % (id_type, _id),
+    headers = {'Accept': 'application/json'})
+
 def get_data(doi = None, pmid = None, dois = None, pmids = None):
   id_type, ids = handle_user_input(doi, pmid, dois, pmids)
   result_df = pd.DataFrame({})
   
-  for i, _id in enumerate(ids):
+  for _id in ids:
     try:
-      response = requests.get(lambda id_type, _id:
-      'https://api.altmetric.com/v1/%s/%s' % (id_type, _id),
-        headers = {'Accept': 'application/json'})
+      response = get_response()
       
-      # if response.status_code == 429:
-      #   print("Rate is limited and you finished your rate")
-      #   time.sleep(3600)
-      #   i = i - 1
-      #   continue
-      # elif response.status_code == 502:
-      #   print("API under  maintenance")
-      #   time.sleep(3600)
-      #   i = i - 1
-      #   continue
+      while response.status_code == 429:
+        logging.error('Rate is limited and you finished your rate')
+        time.sleep(3600)
+        response = get_response()
+      
+      while response.status_code == 502:
+        logging.error('API under maintenance')
+        time.sleep(3600)
+        response = get_response()
       
       json_data = json.loads(response.text.encode('utf-8'))
     
     except:
       logging.error('Data unavailable for %s:%s due to %s' % (id_type, _id, sys.exc_info()[0].__name__))
       json_data = {}
-
+    
     result_df = result_df.append(construct_data_frame(json_data, _id))
   
   return result_df
